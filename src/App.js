0db82e43a20af0f3a09f47ff290fbfb66291e20b
hakeema51.alsoudi@gmail.com <hakeema51.alsoudi@gmail.com>
2023-11-11 18:13:02 +0300

    first App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React, { useState } from "react";
+
+const styleCard = {
+  margin: "3rem",
+  padding: "5px",
+  border: "2px solid gray",
+  background: "lightgray",
+  textAlign: "center",
+};
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container"
+      style={styleCard}>
+      <div className="card">
+        <div className="card-body">
+          <h1>Counter App</h1>
+          <div className="count2">
+            <h2>{count}</h2>
+            <button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+              style={{
+                background: "green",
+                margin: "20px",
+                padding: "10px",
+                borderRadius: "5px",
+              }}>
+              Increment!
+            </button>
+            <button
+              onClick={() => {
+                setCount(count - 1);
+              }}
+              style={{
+                background: "red",
+                margin: "20px",
+                padding: "10px",
+                borderRadius: "5px",
+              }}>
+              Decrement!
+            </button>
+            <button
+              onClick={() => {
+                setCount(0);
+              }}
+              style={{
+                background: "lightblue",
+                margin: "20px",
+                padding: "10px",
+                borderRadius: "5px",
+              }}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
